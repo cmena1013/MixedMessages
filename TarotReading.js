3223eb3f124bc91743db191b626d7e1cf6c862b3
cmena1013 <cmena1013@gmail.com>
2021-10-28 00:25:20 -0400

    Reading files and trying to create object instances.

--- TarotReading.js
+++ TarotReading.js
@@ -1,12 +1,32 @@
 var _ = require('lodash');
+var fs = require('fs');
+const tarotFolder = './Cards';
+const tarotCards = [];
+const tarotTextByLine = [];
+
+// Array of tarot card file names
+let tarotFiles = [];
+
+fs.readdirSync(tarotFolder).forEach(file => {
+    tarotFiles.push(file);
+});
+
+//console.log(tarotFiles);
+
+// Read files
+for (let j = 0; j < tarotFiles.length; j++) {
+    var text = fs.readFileSync(`${tarotFolder}/${tarotFiles[j]}`, 'utf-8');
+    var textByLine = text.split('\n');
+    tarotTextByLine.push(textByLine);
+}
 
 // Read file
-var fs = require('fs');
-var text = fs.readFileSync('./Tarot.txt', 'utf-8');
-var textByLine = text.split('\n');
-var tarotCards = [];
+//var fs = require('fs');
+//var text = fs.readFileSync('./Cards/theFool.txt', 'utf-8');
+//var textByLine = text.split('\n');
+//var tarotCards = [];
 
-// Combine for loop into TarotCard func. Pass str array loop, then assign.
+// Tarot card factory
 function TarotCard (textArray) {
     this.name;
     this.pastUpright;
@@ -25,7 +45,8 @@ function TarotCard (textArray) {
         switch (fileArray[i][0]) {
             case 'name':
                 this.name = fileArray[i][1];
-                tarotCards.push(_.camelCase(fileArray[i][1]));
+                //tarotCards.push(_.camelCase(fileArray[i][1]));
+                console.log(`Brewing ${this.name}`);
                 break;
             case 'pastUpright':
                 this.pastUpright = fileArray[i][1];
@@ -49,5 +70,16 @@ function TarotCard (textArray) {
     }
 }
 
-let theFool = new TarotCard(textByLine);
-console.log(tarotCards);
+// Why is it brewing but not existing?
+for (j = 0; j < tarotTextByLine.length; j++) {
+    let card = tarotTextByLine[j][0].split(':');
+    let cardName = _.camelCase(card[1]);
+    //console.log(`Sending ${cardName} to brew`);
+    cardName = new TarotCard(tarotTextByLine[j]);
+    //console.log(tarotTextByLine[j]);
+    //tarotCards.push(cardName);
+    console.log(death.name);
+}
+//console.log(tarotCards);
+//let theFool = new TarotCard(textByLine);
+//console.log(tarotCards);
