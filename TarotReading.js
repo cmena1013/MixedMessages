d68b1f787959efd9322a855a40f8c115f61883a3
cmena1013 <cmena1013@gmail.com>
2021-10-29 09:37:06 -0400

    Update TarotReading.js

--- TarotReading.js
+++ TarotReading.js
@@ -12,74 +12,46 @@ fs.readdirSync(tarotFolder).forEach(file => {
 });
 
 //console.log(tarotFiles);
+let cardsDrawn = [];
+while (cardsDrawn.length < 3){
+    var r = Math.floor(Math.random() * tarotFiles.length);
+    if(cardsDrawn.indexOf(r) === -1) cardsDrawn.push(r);
+}
 
+let pastPresentFuture = [];
 // Read files
-for (let j = 0; j < tarotFiles.length; j++) {
-    var text = fs.readFileSync(`${tarotFolder}/${tarotFiles[j]}`, 'utf-8');
+for (let j = 0; j < cardsDrawn.length; j++) {
+    var text = fs.readFileSync(`${tarotFolder}/${tarotFiles[cardsDrawn[j]]}`, 'utf-8');
     var textByLine = text.split('\n');
-    tarotTextByLine.push(textByLine);
-}
-
-// Read file
-//var fs = require('fs');
-//var text = fs.readFileSync('./Cards/theFool.txt', 'utf-8');
-//var textByLine = text.split('\n');
-//var tarotCards = [];
-
-// Tarot card factory
-function TarotCard (textArray) {
-    this.name;
-    this.pastUpright;
-    this.pastReverse;
-    this.presentUpright;
-    this.presentReverse;
-    this.futureUpright;
-    this.futureReverse;
-    let fileArray = [];
-
-    for (let i = 0; i < textArray.length; i++ ) {
-        fileArray.push(textArray[i].split(':'));
+    //tarotTextByLine.push(textByLine);
+    let uprightOrReverse = Math.round(Math.random());
+    let message;
+    
+    switch (j) {
+        case 0:
+            message = `The past: ${textByLine[0]}`;
+            if (uprightOrReverse === 0) {
+                message += `\n${textByLine[1]}`;
+                break;
+            }
+            message += `\n${textByLine[2]}`;
+            break;
+        case 1:
+            message = `The present: ${textByLine[0]}`;
+            if (uprightOrReverse === 0) {
+                message += `\n${textByLine[3]}`;
+                break;
+            }
+            message += `\n${textByLine[4]}`;
+            break;
+        case 2:
+            message = `The future: ${textByLine[0]}`;
+            if (uprightOrReverse === 0) {
+                message += `\n${textByLine[5]}`;
+                break;
+            }
+            message += `\n${textByLine[6]}`;
+            break;
     }
-
-    for (i = 0; i < fileArray.length; i++) {
-        switch (fileArray[i][0]) {
-            case 'name':
-                this.name = fileArray[i][1];
-                //tarotCards.push(_.camelCase(fileArray[i][1]));
-                console.log(`Brewing ${this.name}`);
-                break;
-            case 'pastUpright':
-                this.pastUpright = fileArray[i][1];
-                break;
-            case 'pastReverse':
-                this.pastReverse = fileArray[i][1];
-                break;
-            case 'presentUpright':
-                this.presentUpright = fileArray[i][1];
-                break;
-            case 'presentReverse':
-                this.presentReverse = fileArray[i][1];
-                break;
-            case 'futureUpright':
-                this.futureUpright = fileArray[i][1];
-                break;
-            case 'futureReverse':
-                this.futureReverse = fileArray[i][1];
-                break;
-        }
-    }
+    console.log(message);
 }
-
-// Why is it brewing but not existing?
-for (j = 0; j < tarotTextByLine.length; j++) {
-    let card = tarotTextByLine[j][0].split(':');
-    let cardName = _.camelCase(card[1]);
-    //console.log(`Sending ${cardName} to brew`);
-    cardName = new TarotCard(tarotTextByLine[j]);
-    //console.log(tarotTextByLine[j]);
-    //tarotCards.push(cardName);
-    console.log(death.name);
-}
-//console.log(tarotCards);
-//let theFool = new TarotCard(textByLine);
-//console.log(tarotCards);
